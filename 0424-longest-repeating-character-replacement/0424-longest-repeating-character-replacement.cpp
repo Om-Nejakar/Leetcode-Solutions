105a0ffa9772287619e68fd73fbf4ba6bac42b8c
Om Nejakar <omnejakar1103@gmail.com>
2026-03-16 07:13:35 +0530

    Time: 4 ms (56%), Space: 10.8 MB (92.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
+    //2 pointer
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> hashmap;
-        int window_size = 0, left = 0, max_count = 0, len = 0;
+        int n = s.length();
+        int maxlen = 0;
+        int hp[26] = {0};
+        int maxfreq = 0;
 
-        for(int right=0;right<s.length();right++) {
-            hashmap[s[right]] += 1; // store the count in the map
-            max_count = max(max_count, hashmap[s[right]]); // take the highest freq count 
+        int left = 0;
+        int right = 0;
 
-            window_size = right - left + 1;
-            if((window_size - max_count) > k) { // its like win = frq + k (>k it means that it has more variable hence reduce the size fromm left )
-                hashmap[s[left]] -= 1;
+        while(right < n) {
+            hp[s[right]-'A']++;
+            maxfreq = max(maxfreq, hp[s[right]-'A']);
+
+            while((right-left+1)-maxfreq > k) {
+                hp[s[left]-'A']--;
+                maxfreq = 0;
+
+                for(int i=0;i<26;i++) {
+                    maxfreq = max(maxfreq, hp[i]);
+                }
                 left++;
             }
 
-            len = max(len, right-left+1); // make the length comparision 
+            if((right-left+1)-maxfreq <= k) {
+                maxlen = max(maxlen, right-left+1);
+            }
+
+            right++;
         }
-        return len;
+
+        return maxlen;
     }
 };
