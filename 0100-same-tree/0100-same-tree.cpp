bc9dca34bfb175819e219007ac35174488e3d83f
Om Nejakar <omnejakar1103@gmail.com>
2026-03-28 19:56:27 +0530

    Time: 0 ms (100%), Space: 12.8 MB (46.98%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,43 +11,22 @@
  */
 class Solution {
 public:
-    bool leafNode(TreeNode* root) {
-        if(!root->left  && !root->right) {
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) {
             return true;
         }
-        return false;
-    }
-    bool checkSame(queue<TreeNode*>& Q1, queue<TreeNode*>& Q2) {
-        while(!Q1.empty() && !Q2.empty()) {
-            TreeNode* q1node = Q1.front(); Q1.pop();
-            TreeNode* q2node = Q2.front(); Q2.pop();
 
-            if(!q1node && !q2node) continue;
-            if(!q1node || !q2node) {
-                return false;
-            }
-            if(q1node->val != q2node->val) {
-                return false;
-            }
-
-           
-            Q1.push(q1node->left);
-            Q2.push(q2node->left);
-            Q1.push(q1node->right);
-            Q2.push(q2node->right);
-            
+        if((p == nullptr && q != nullptr) || (p != nullptr && q == nullptr)) {
+            return false;
         }
-        return  Q1.empty() && Q2.empty();
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        queue<TreeNode*> Q1, Q2;
 
-        if(!p && q || p && !q) {
-            return  false;
+        if(p->val != q->val) {
+            return false;
         }
-        Q1.push(p);
-        Q2.push(q);
 
-        return checkSame(Q1, Q2);
+        bool leftside = isSameTree(p->left, q->left);
+        bool rigthside = isSameTree(p->right, q->right);
+
+        return leftside&&rigthside;
     }
 };
