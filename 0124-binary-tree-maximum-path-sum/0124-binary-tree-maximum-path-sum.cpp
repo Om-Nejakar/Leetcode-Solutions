1da6f76aab0fdc0ca8fcc9c08da54e19433bd050
Om Nejakar <omnejakar1103@gmail.com>
2026-03-28 18:11:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,21 +11,22 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root, int& sum) {
+    int f(TreeNode* root, int& sum) {
         if(root == nullptr) {
             return 0;
         }
 
-        int lh = max(0, height(root->left, sum));
-        int rh = max(0, height(root->right, sum));
+        int leftheight = max(0, f(root->left, sum));
+        int rightheight = max(0, f(root->right, sum));
 
-        sum = max(sum, lh+rh+root->val);
+        sum = max(sum, leftheight+rightheight+root->val); 
+         
+        return max(leftheight, rightheight) + root->val;
 
-        return max(lh, rh) + root->val;
     }
     int maxPathSum(TreeNode* root) {
-        int sum = INT_MIN;
-        height(root, sum);
+        int sum = 0;
+        f(root, sum);
         return sum;
     }
 };
